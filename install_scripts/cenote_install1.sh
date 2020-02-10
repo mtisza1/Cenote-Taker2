8b5542e9f6055b888c6a8268c21c046028d623b5
mtisza1 <michael.tisza@gmail.com>
2020-02-10 17:19:45 -0500

    building toward conda compatibility 1.1

--- install_scripts/cenote_install1.sh
+++ install_scripts/cenote_install1.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
 
-git clone https://github.com/mtisza1/Cenote-Taker2.git
+if [ -s Cenote-Taker2/run_cenote-taker2_200207.py ] ; then
+	cd Cenote-Taker2
+	git pull
+else
+	git clone https://github.com/mtisza1/Cenote-Taker2.git
 
-cd Cenote-Taker2
+	cd Cenote-Taker2
+fi
 #chmod +x irf307.linux.exe
 # cloning PHANOTATE
-git clone --recursive https://github.com/deprekate/PHANOTATE.git
-cd PHANOTATE; # git checkout version from feb 8 2020
-make
-cd ..
+if [ -s PHANOTATE/phanotate.py ] ; then
+	echo "phanotate already present"
+else
+	git clone --recursive https://github.com/deprekate/PHANOTATE.git
+	cd PHANOTATE; # git checkout version from feb 8 2020
+	make
+	cd ..
+fi
 
+if [ -s last-1047/README.txt ] ; then
+	echo "last already present"
+else
+	wget http://last.cbrc.jp/last-1047.zip
+	unzip last-1047.zip
+	cd last-1047/
+	make
+	cd ..
+	rm last-1047.zip
+fi
 
-wget http://last.cbrc.jp/last-1047.zip
-unzip last-1047.zip
-cd last-1047/
-make
-cd ..
-
-CONDA_BASE=$( conda info --base )
 eval "$(conda shell.bash hook)"
+conda info --envs | if grep -q "Cenote-Taker2" ; then
+	conda env remove --name Cenote-Taker2
+fi
 conda env create --file cenote-taker2_env.yml
 #conda create -n Cenote-Taker2 -c defaults -c bioconda -c AgBiome --no-channel-priority python=3.6 prodigal=2.6.3 BWA=0.7.17 samtools=1.3 mummer=3.23 circlator=1.5.5 blast=2.9.0 bioawk=1.0 entrez-direct=13.3 krona=2.7.1 hmmer=3.3 bowtie2=2.3.5 trnascan-se=2.0.5 bbtools tbl2asn=25.7 emboss=6.6.0 cmake numpy pandas matplotlib 
 conda activate Cenote-Taker2
@@ -30,57 +45,86 @@ else
 	exit 
 fi
 # getting hh-suite from github. The anaconda package of hh-suite causes conflicts
-git clone https://github.com/soedinglab/hh-suite.git
-cd hh-suite # git checkout version from feb 8 2020
-mkdir -p build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=. ..
-make -j 4 && make install
-export PATH="$(pwd)/bin:$(pwd)/scripts:$PATH"
-cd ..
-cd ..
+if [ -s hh-suite/README.md ] ; then
+	echo "hh-suite already present"
+else
+	git clone https://github.com/soedinglab/hh-suite.git
+	cd hh-suite # git checkout version from feb 8 2020
+	mkdir -p build && cd build
+	cmake -DCMAKE_INSTALL_PREFIX=. ..
+	make -j 4 && make install
+	export PATH="$(pwd)/bin:$(pwd)/scripts:$PATH"
+	cd ..
+	cd ..
+fi
 
 
 
-CT2_DIRE=$PWD
-KRONA_DIRE=$( which python | sed 's/bin\/python/opt\/krona/g' )
-cd ${KRONA_DIRE}
+CT2_DIR=$PWD
+KRONA_DIR=$( which python | sed 's/bin\/python/opt\/krona/g' )
+cd ${KRONA_DIR}
 sh updateTaxonomy.sh
-cd ${KRONA_DIRE}
+cd ${KRONA_DIR}
 sh updateAccessions.sh
-cd ${CT2_DIRE}
+cd ${CT2_DIR}
 
 #wget hmmer DBs
-wget https://zenodo.org/record/3660539/files/hmmscan_DBs.tgz
-tar -xvf hmmscan_DBs.tgz
-rm hmmscan_DBs.tgz
+if [ -s hmmscan_DBs/useful_hmms_baits_and_not2a.h3p ] ; then
+	echo "HMM databases appear to be already present"
+else
+	wget https://zenodo.org/record/3660539/files/hmmscan_DBs.tgz
+	tar -xvf hmmscan_DBs.tgz
+	rm hmmscan_DBs.tgz
+fi
 #wget BLAST DBs
-wget https://zenodo.org/record/3660538/files/blast_DBs.tgz
-tar -xvf blast_DBs.tgz
-rm blast_DBs.tgz
+if [ -s blast_DBs/virus_adinto_polinton_prot_190925.psq ] ; then
+	echo "BLAST databases appear to be already present"
+else
+	wget https://zenodo.org/record/3660538/files/blast_DBs.tgz
+	tar -xvf blast_DBs.tgz
+	rm blast_DBs.tgz
+fi
 #wget hhsuite DBs
 ## NCBI_CD
-wget https://zenodo.org/record/3660537/files/NCBI_CD_hhsuite.tgz
-tar -xvf NCBI_CD_hhsuite.tgz
-rm NCBI_CD_hhsuite.tgz
+if [ -s NCBI_CD/NCBI_CD_a3m.ffdata ] ; then
+	echo "NCBI_CD hhsuite databases appear to be already present"
+else	
+	wget https://zenodo.org/record/3660537/files/NCBI_CD_hhsuite.tgz
+	tar -xvf NCBI_CD_hhsuite.tgz
+	rm NCBI_CD_hhsuite.tgz
+fi
 ## pfam_32
-mkdir pfam_32_db && cd pfam_32_db
-wget http://wwwuser.gwdg.de/~compbiol/data/hhsuite/databases/hhsuite_dbs/pfamA_32.0.tar.gz
-tar -xvf pfamA_32.0.tar.gz
-rm pfamA_32.0.tar.gz
-cd ..
+if [ -s pfam_32_db/pfam_a3m.ffdata ] ; then
+	echo "pfam_32 hhsuite databases appear to be already present"
+else
+	mkdir pfam_32_db && cd pfam_32_db
+	wget http://wwwuser.gwdg.de/~compbiol/data/hhsuite/databases/hhsuite_dbs/pfamA_32.0.tar.gz
+	tar -xvf pfamA_32.0.tar.gz
+	rm pfamA_32.0.tar.gz
+	cd ..
+fi
 ## pdb70_latest
-mkdir pdb70 && cd pdb70
-wget http://wwwuser.gwdg.de/~compbiol/data/hhsuite/databases/hhsuite_dbs/pdb70_from_mmcif_latest.tar.gz
-tar -xvf pdb70_from_mmcif_latest.tar.gz
-rm pdb70_from_mmcif_latest.tar.gz
-cd ..
+if [ -s pdb70/pdb70_a3m.ffdata ] ; then
+	echo "pdb70 hhsuite databases appear to be already present"
+else
+	mkdir pdb70 && cd pdb70
+	wget http://wwwuser.gwdg.de/~compbiol/data/hhsuite/databases/hhsuite_dbs/pdb70_from_mmcif_latest.tar.gz
+	tar -xvf pdb70_from_mmcif_latest.tar.gz
+	rm pdb70_from_mmcif_latest.tar.gz
+	cd ..
+fi
 #wget cdd rpsblast db
-mkdir cdd_rps_db && cd cdd_rps_db
-wget ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/little_endian/Cdd_LE.tar.gz
-tar -xvf Cdd_LE.tar.gz
-rm Cdd_LE.tar.gz
+if [ -s cdd_rps_db/Cdd.rps ] ; then
+	echo "RPSBLAST CDD databases appear to be already present"
+else
+	mkdir cdd_rps_db && cd cdd_rps_db
+	wget ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/little_endian/Cdd_LE.tar.gz
+	tar -xvf Cdd_LE.tar.gz
+	rm Cdd_LE.tar.gz
+	cd ..
+fi
 
-echo "Cenote-Taker2 should now run. Use \'python /path/to/Cenote-Taker2/run_cenote-taker2_200207.py\'"
+echo "Cenote-Taker2 should now run. Use: python /path/to/Cenote-Taker2/run_cenote-taker2_200207.py"
 #python setup.py build; python setup.py install
 
 #Cenote-Taker2 -h
