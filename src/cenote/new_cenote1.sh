6d50b65677bc1581677a4f3c32829fa30159fb9e
Mike Tisza <michael.tisza@gmail.com>
2023-07-26 18:34:13 -0500

    adding terminal repeats and rotate

--- src/cenote/new_cenote1.sh
+++ src/cenote/new_cenote1.sh
@@ -14,6 +14,7 @@ LIN_MINIMUM_DOMAINS=2
 CIRC_MINIMUM_DOMAINS=1
 MEM=25
 CPU=12
+WRAP="True"
 
 CENOTE_DBs="/Users/u241374/mike_tisza/cmmr_repos/Cenote-Taker2"
 CENOTE_SCRIPTS="/Users/u241374/mike_tisza/cmmr_repos/Cenote-Taker2/src/cenote"
@@ -95,7 +96,7 @@ fi
 
 
 ## split contigs into equal parts for prodigal ORF calling
-if [ -s ${run_title}/${run_title}.contigs_over_${LENGTH_MINIMUM}nt.fasta ]
+if [ -s ${run_title}/${run_title}.contigs_over_${LENGTH_MINIMUM}nt.fasta ] ; then
 	if [ ! -d "${TEMP_DIR}/split_orig_contigs" ]; then
 		mkdir ${TEMP_DIR}/split_orig_contigs
 	fi
@@ -145,19 +146,23 @@ else
 	HALLMARK_MINIMUM=$CIRC_MINIMUM_DOMAINS
 fi
 
+## grabbing contigs with minimum marker gene number
 if [ -s ${TEMP_DIR}/split_orig_pyhmmer/contig_hallmark_count.tsv ] ; then
-	KEEP_COUNT=$( tail -n+2 ${TEMP_DIR}/split_orig_pyhmmer/contig_hallmark_count.tsv |\
-	  awk -v min="${HALLMARK_MINIMUM}" '{OFS=FS="\t"}{if ($2 >=) {print}' | wc -l )
+
+	MDYT=$( date +"%m-%d-%y---%T" )
+	echo "grabbing contigs with minimum marker gene number " $MDYT
+
+	KEEP_COUNT=$( tail -n+2 ${TEMP_DIR}/split_orig_pyhmmer/contig_hallmark_count.tsv | awk -v min="${HALLMARK_MINIMUM}" '{OFS=FS="\t"}{if ($2 >= min) {print}}' | wc -l )
 
 	if [ $KEEP_COUNT -ge 1 ] ; then
-		echo "found ${KEEP_COUNT} contig(s) with at least ${KEEP_COUNT} hallmark gene(s)"
+		echo "found ${KEEP_COUNT} contig(s) with at least ${HALLMARK_MINIMUM} hallmark gene(s)"
 		echo "analyzing these contigs further..."
 
 		tail -n+2 ${TEMP_DIR}/split_orig_pyhmmer/contig_hallmark_count.tsv |\
-		  awk -v min="${HALLMARK_MINIMUM}" '{OFS=FS="\t"}{if ($2 >=) {print $1}' > ${TEMP_DIR}/split_orig_pyhmmer/contigs_to_keep.txt
+		  awk -v min="${HALLMARK_MINIMUM}" '{OFS=FS="\t"}{if ($2 >= min) {print $1}}' > ${TEMP_DIR}/split_orig_pyhmmer/contigs_to_keep.txt
 
 		seqkit grep -f ${TEMP_DIR}/split_orig_pyhmmer/contigs_to_keep.txt\
-		  ${run_title}/${run_title}.contigs_over_${LENGTH_MINIMUM}nt.fasta > ${run_title}/${run_title}.hallmark_contigs.fasta
+		  ${run_title}/${run_title}.contigs_over_${LENGTH_MINIMUM}nt.fasta > ${TEMP_DIR}/unprocessed_hallmark_contigs.fasta
 
 	else
 		echo "no contigs with ${KEEP_COUNT} hallmark gene(s) were detected. Exiting."
@@ -167,6 +172,100 @@ else
 	echo "couldn't find ${TEMP_DIR}/split_orig_pyhmmer/contig_hallmark_count.tsv. Exiting."
 	exit
 fi
+
+## detecting DTRs and ITRs. Trimming DTRs
+if [ -s ${TEMP_DIR}/unprocessed_hallmark_contigs.fasta ] ; then
+
+	MDYT=$( date +"%m-%d-%y---%T" )
+	echo "detecting DTRs and ITRs " $MDYT
+
+	python ${CENOTE_SCRIPTS}/python_modules/terminal_repeats.py ${TEMP_DIR}/unprocessed_hallmark_contigs.fasta ${TEMP_DIR} $WRAP
+
+else
+	echo "couldn't find hallmark contigs at ${TEMP_DIR}/unprocessed_hallmark_contigs.fasta"
+	exit
+fi
+
+## Rotating DTR contigs
+if [ -s ${TEMP_DIR}/trimmed_TRs_hallmark_contigs.fasta ] && [ -s ${TEMP_DIR}/hallmark_contigs_terminal_repeat_summary.tsv ] ; then
+	if [ "$WRAP" == "True" ] ; then
+
+		MDYT=$( date +"%m-%d-%y---%T" )
+		echo "rotating DTR contigs " $MDYT
+
+		if [ ! -d "${TEMP_DIR}/rotation" ]; then
+			mkdir ${TEMP_DIR}/rotation
+		fi
+
+		awk '{OFS=FS="\t"}{ if (NR != 1 && $4 != "NA") {print $1, $3} }' ${TEMP_DIR}/hallmark_contigs_terminal_repeat_summary.tsv |\
+		  while read DTR_CONTIG TRIM_LENGTH ; do
+			seqkit grep -p "${DTR_CONTIG}" ${TEMP_DIR}/trimmed_TRs_hallmark_contigs.fasta |\
+			  prodigal -a ${TEMP_DIR}/rotation/${DTR_CONTIG}.unrotated.faa -i /dev/stdin -c -p meta -q >/dev/null 2>&1
+			FWD_GENES=$( grep "^>" ${TEMP_DIR}/rotation/${DTR_CONTIG}.unrotated.faa | sed 's/ # /	/g' |\
+				awk '{FS=OFS="\t"}{if ($0 ~ "partial=00;start_type" && $4 == 1) {print $4}}' | wc -l )
+			REV_GENES=$( grep "^>" ${TEMP_DIR}/rotation/${DTR_CONTIG}.unrotated.faa | sed 's/ # /	/g' |\
+				awk '{FS=OFS="\t"}{if ($0 ~ "partial=00;start_type" && $4 == -1) {print $4}}' | wc -l )
+			if [ $FWD_GENES -ge $REV_GENES ] && [ $FWD_GENES -ge 1 ]; then
+				START_BASE=$( grep "^>" ${TEMP_DIR}/rotation/${DTR_CONTIG}.unrotated.faa | sed 's/ # /	/g' |\
+					awk '{FS=OFS="\t"}{if ($0 ~ "partial=00;start_type" && $4 == 1) {print $2, ($3-$2)}}' |\
+					sort -rg -k2,2 | head -n1 | cut -f1 )
+				seqkit grep -p "${DTR_CONTIG}" ${TEMP_DIR}/trimmed_TRs_hallmark_contigs.fasta |\
+				  seqkit restart -i ${START_BASE} > ${TEMP_DIR}/rotation/${DTR_CONTIG}.rotate.fasta
+			elif [ $REV_GENES -ge 1 ]; then
+				seqkit grep -p "${DTR_CONTIG}" ${TEMP_DIR}/trimmed_TRs_hallmark_contigs.fasta |\
+				  seqkit seq --quiet -t DNA -r -p > ${TEMP_DIR}/rotation/${DTR_CONTIG}.rc.fna
+				prodigal -a ${TEMP_DIR}/rotation/${DTR_CONTIG}.rc.faa -i ${TEMP_DIR}/rotation/${DTR_CONTIG}.rc.fna -p meta -q >/dev/null 2>&1
+				RC_FWD_GENES=$( grep "^>" ${TEMP_DIR}/rotation/${DTR_CONTIG}.rc.faa | sed 's/ # /	/g' |\
+					awk '{FS=OFS="\t"}{if ($0 ~ "partial=00;start_type" && $4 == 1) {print $4}}' | wc -l )
+				if [ $RC_FWD_GENES -ge 1 ] ; then 
+					START_BASE=$( grep "^>" ${TEMP_DIR}/rotation/${DTR_CONTIG}.rc.faa | sed 's/ # /	/g' |\
+						awk '{FS=OFS="\t"}{if ($0 ~ "partial=00;start_type" && $4 == 1) {print $2, ($3-$2)}}' |\
+						sort -rg -k2,2 | head -n1 | cut -f1 )
+					cat ${TEMP_DIR}/rotation/${DTR_CONTIG}.rc.fna |\
+					  seqkit restart -i ${START_BASE} > ${TEMP_DIR}/rotation/${DTR_CONTIG}.rotate.fasta
+				else
+					echo "Can't find suitable ORF to set rotation of ${DTR_CONTIG} and will remain unrotated"
+					seqkit grep -p "${DTR_CONTIG}" ${TEMP_DIR}/trimmed_TRs_hallmark_contigs.fasta > ${TEMP_DIR}/rotation/${DTR_CONTIG}.rotate.fasta
+				fi
+			else
+				echo "Can't find suitable ORF to set rotation of $nucl_fa and will remain unrotated"
+				seqkit grep -p "${DTR_CONTIG}" ${TEMP_DIR}/trimmed_TRs_hallmark_contigs.fasta > ${TEMP_DIR}/rotation/${DTR_CONTIG}.rotate.fasta
+			fi
+		done
+
+		awk '{OFS=FS="\t"}{ if (NR != 1 && $4 == "NA") {print $1} }' ${TEMP_DIR}/hallmark_contigs_terminal_repeat_summary.tsv > ${TEMP_DIR}/hallmark_contigs_no_DTRS.txt
+
+		if [ -s ${TEMP_DIR}/hallmark_contigs_no_DTRS.txt ] ; then
+			seqkit grep -f ${TEMP_DIR}/hallmark_contigs_no_DTRS.txt\
+			  ${TEMP_DIR}/trimmed_TRs_hallmark_contigs.fasta > ${TEMP_DIR}/hallmark_contigs_no_DTRS.fasta
+		fi
+
+		ALL_DTRS=$( find ${TEMP_DIR}/rotation -type f -name "*.rotate.fasta" )
+
+		if [ -n "$ALL_DTRS" ] ; then
+			echo "$ALL_DTRS" | while read SEQ ; do
+				cat $SEQ
+			done > ${TEMP_DIR}/hallmark_contigs_with_DTRS.rotated.fasta
+		fi
+
+		if [ -s ${TEMP_DIR}/hallmark_contigs_with_DTRS.rotated.fasta ] ; then
+			cat ${TEMP_DIR}/hallmark_contigs_with_DTRS.rotated.fasta >> ${TEMP_DIR}/oriented_hallmark_contigs.fasta
+		fi
+
+		if [ -s ${TEMP_DIR}/hallmark_contigs_no_DTRS.fasta ] ; then
+			cat ${TEMP_DIR}/hallmark_contigs_no_DTRS.fasta >> ${TEMP_DIR}/oriented_hallmark_contigs.fasta
+		fi
+
+
+	else
+		echo "not wrapping"
+		
+		cp ${TEMP_DIR}/trimmed_TRs_hallmark_contigs.fasta ${TEMP_DIR}/oriented_hallmark_contigs.fasta
+	fi
+else
+	echo "couldn't find hallmark contigs with processed terminal repeats at ${TEMP_DIR}/trimmed_TRs_hallmark_contigs.fasta"
+fi
+
 
 
 
---
+++ src/cenote/python_modules/terminal_repeats.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.SeqFeature import SeqFeature, FeatureLocation
+import os
+import re
+import sys
+import pandas as pd
+
+fasta_file = sys.argv[1]
+
+out_dir = sys.argv[2]
+
+if not os.path.isdir(out_dir):
+    os.makedirs(out_dir)
+
+output_file = os.path.join(out_dir, "trimmed_TRs_hallmark_contigs.fasta")
+
+if os.path.isfile(output_file):
+    os.remove(output_file)
+
+wrap = sys.argv[3]
+
+## The following functions are copied wholesale from CheckV as I find them to be fast and correct
+## link https://bitbucket.org/berkeleylab/checkv/src/master/checkv/modules/complete_genomes.py
+
+def fetch_dtr(fullseq, min_length=20):
+    startseq = fullseq[0:min_length]
+    # find index positions of all matches of startseq in fullseq
+    # only keep matches occuring in 2nd half of string
+    matches = [
+        m.start() for m in re.finditer("(?={0})".format(re.escape(startseq)), fullseq)
+    ]
+    matches = [_ for _ in matches if _ >= len(fullseq) / 2]
+    for matchpos in matches:
+        # determine if the match extends to the contig end
+        endseq = fullseq[matchpos:]
+        if fullseq[0 : len(endseq)] == endseq:
+            return endseq
+    return ""
+
+def reverse_complement(seq):
+    if sys.version_info > (3, 0):
+        trans = str.maketrans("ACTG", "TGAC")
+    else:
+        trans = string.maketrans("ACTG", "TGAC")
+    return seq[::-1].translate(trans)
+
+def fetch_itr(seq, min_len=20, max_len=1000):
+    rev = reverse_complement(seq)
+    # see if minimal substring occurs at end
+    if seq[:min_len] == rev[:min_len]:
+        # extend to maximum substring, up to <max_len>
+        i = min_len + 1
+        while seq[:i] == rev[:i] and i <= max_len:
+            i += 1
+        return seq[: i - 1]
+    # no match
+    else:
+        return ""
+
+
+
+
+terminal_r_list = []
+for seq_record in SeqIO.parse(fasta_file, "fasta"):
+    dtr_seq = fetch_dtr(str(seq_record.seq))
+
+    if not dtr_seq:
+        dtr_seq = "NA"
+
+    itr_seq = fetch_itr(str(seq_record.seq))
+
+    if not itr_seq:
+        itr_seq = "NA"
+
+    if not dtr_seq == "NA" and wrap.lower() == "true":
+        print(f">{seq_record.id}", file = open(output_file, "a"))
+        print(seq_record.seq[:-len(dtr_seq)], file = open(output_file, "a"))
+
+        terminal_r_list.append([seq_record.id, len(seq_record.seq), len(seq_record.seq[:-len(dtr_seq)]), dtr_seq, itr_seq])
+    else:
+        print(f">{seq_record.id}", file = open(output_file, "a"))
+        print(seq_record.seq, file = open(output_file, "a"))
+
+        terminal_r_list.append([seq_record.id, len(seq_record.seq), len(seq_record.seq[:-len(dtr_seq)]), dtr_seq, itr_seq])
+
+terminal_df = pd.DataFrame(terminal_r_list, columns=["contig", "in_length_contig", "out_length_contig", "dtr_seq", "itr_seq"])
+
+output_table = os.path.join(out_dir, "hallmark_contigs_terminal_repeat_summary.tsv")
+
+terminal_df.to_csv(output_table,
+                        sep = "\t", index = False)
